a5051baa47eb5fc121758dbb09becf431f7720b9
atan4-quantium <atan4@woolworths.com.au>
2024-09-14 22:30:56 +1000

    add: vertexai

--- pr_agent/servers/github_action_runner.py
+++ pr_agent/servers/github_action_runner.py
@@ -37,6 +37,8 @@ async def run_action():
     OPENAI_KEY = os.environ.get('OPENAI_KEY') or os.environ.get('OPENAI.KEY')
     OPENAI_ORG = os.environ.get('OPENAI_ORG') or os.environ.get('OPENAI.ORG')
     GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
+    VERTEX_PROJECT = os.environ.get('VERTEX_PROJECT')
+    VERTEX_LOCATION = os.environ.get('VERTEX_LOCATION')
     # get_settings().set("CONFIG.PUBLISH_OUTPUT_PROGRESS", False)
 
     # Check if required environment variables are set
@@ -58,6 +60,11 @@ async def run_action():
         print("OPENAI_KEY not set")
     if OPENAI_ORG:
         get_settings().set("OPENAI.ORG", OPENAI_ORG)
+        
+    if VERTEX_PROJECT:
+        get_settings().set("VERTEXAI.VERTEX_PROJECT", VERTEX_PROJECT)
+        get_settings().set("VERTEXAI.VERTEX_LOCATION", VERTEX_LOCATION)
+
     get_settings().set("GITHUB.USER_TOKEN", GITHUB_TOKEN)
     get_settings().set("GITHUB.DEPLOYMENT_TYPE", "user")
     enable_output = get_setting_or_env("GITHUB_ACTION_CONFIG.ENABLE_OUTPUT", True)
